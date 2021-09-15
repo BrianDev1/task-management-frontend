f7db13e9367ec7f55c16a4c3f100db1ccadb9561
BrianDev1 <BrianDev1@users.noreply.github.com>
2021-09-15 22:41:45 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.4782a6df33e615523546ba62cf737952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e9fd60ab9f54b01f259e2bf00cf08f",
+    "url": "https://BrianDev1.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "6d0109f8d161cc8911a1",
+    "url": "https://BrianDev1.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "74a3471bd3345d1fea8a",
+    "url": "https://BrianDev1.github.io/task-management-frontend/static/js/2.b7f13879.chunk.js"
+  },
+  {
+    "revision": "6d0109f8d161cc8911a1",
+    "url": "https://BrianDev1.github.io/task-management-frontend/static/js/main.2c30a99e.chunk.js"
+  },
+  {
+    "revision": "8a4b3ccdc558c647b603",
+    "url": "https://BrianDev1.github.io/task-management-frontend/static/js/runtime~main.2a9aa817.js"
+  }
+]);
